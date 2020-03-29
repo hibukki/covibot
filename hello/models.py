8b1f84b23566f61d8e0ebd11ac9a130b65975418
Yonatan Cale <myselfandfredy@gmail.com>
2020-03-29 04:01:03 +0300

    Add chat model (and migration) (Also keep the demo stub model)

--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+
+
+class Chats(models.Model):
+    # Docs: https://python-telegram-bot.readthedocs.io/en/stable/telegram.chat.html#telegram.Chat.id
+    chat_id = models.IntegerField("python-telegram-bot chat id", primary_key=True)
+    user_requested_stop = models.BooleanField("User asked to stop", default=False)
+
+
+
